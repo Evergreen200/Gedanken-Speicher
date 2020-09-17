216a1ab067c96e4731972ad63b16d7bf5682741e
Evergreen <evergreen.git@gmail.com>
2020-09-17 18:27:58 +0200

    Test

--- thoughtMemory.py
+++ thoughtMemory.py
@@ -1,19 +1,22 @@
 def return_gedanken():
-  if len(thoughts) == 0:
-    return("Du hast noch keine Gedanken gespeichert!")
-  else:
-    return(thought[0:])
-   
+    if len(thoughts) == 0:
+        return("Du hast noch keine Gedanken gespeichert!")
+    else:
+        return(thoughts[0:])
+
 def commands():
-  command_list = ["gedanken"]
-  return(command_list[0:])
- 
-print("Thought Memory")
+    command_list = ["gedanken"] # later, i will rename all functions and commands
+    return(command_list[0:])
+
+print("Thought Memory") 
 
 thoughts = ["Gedanke", "2. Gedanke"]
 
 if (input() == "gedanken"):
-  print(return_gedanken())
-  
+    print(return_gedanken())
+
 if (input() == "commands" or "help"):
-  print(commands())
+    print(commands()) 
+
+# Test
+
