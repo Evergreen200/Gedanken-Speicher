92e459fef308ccf29d973f0dcac8e3b4349f40d8
Evergreen <evergreen.git@gmail.com>
2020-09-20 08:21:44 +0200

    Implemented quit, help command and refactored the code.

--- thoughtMemory.py
+++ thoughtMemory.py
@@ -1,32 +1,44 @@
-import sys
-
 def return_gedanken():
     if len(thoughts) == 0:
-        return("Du hast noch keine Gedanken gespeichert!")
+        return ("Du hast noch keine Gedanken gespeichert!")
     else:
-        return(thoughts[0:])
-
-def commands():
-    command_list = ["gedanken"] # later, i will rename all functions and commands
-    return(command_list[0:])
-
-def quit():
-    running = False
-    sys.exit()
+        return (thoughts[0:])
 
 thoughts = ["Gedanke", "2. Gedanke"]
 
 running = True
 
+def quit_program():
+    exit(0)
+
+def print_help():
+    print(Commands.keys())
+
+def print_thoughts():
+    pass
+
+def write():
+    pass
+
+def load():
+    pass
+Commands = {
+    'help': print_help,
+    'quit': quit_program,
+    'thoughts': print_thoughts,
+    'write': write,
+    'load': load
+}
+
 if __name__ == '__main__':
-    print("Thought Memory") 
-    # while running:
+    print("Thought Memory")
+    while True:
+        command = input("> ").lower().split(" ")
+        if command[0] in Commands:
+            if len(command) > 1:
+                Commands[command[0]](command[:])
+            else:
+                Commands[command[0]]()
+        else:
+            print("Unknown command")
 
-    if input() == "gedanken":
-        print(return_gedanken())
-    elif input() == "commands" or "help":
-        print(commands()) 
-    elif input() == "exit":
-        quit()
-
-            
